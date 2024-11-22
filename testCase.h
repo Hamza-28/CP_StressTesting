9d2955d195ee9b82c267efa322a199db2f37e96c
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2024-11-22 10:57:01 +0600

    mt19937

--- testCase.h
+++ testCase.h
@@ -6,13 +6,15 @@ void GenerateTestCase() {
   
   cout.open("testCase.txt");
 
-  srand(time(0));
+  mt19937 mt(time(0));      // generates unsigned random number between 0 to 2^32 - 1
+  // mt19937_64 mt(time(0));   // geerates unsigned random number between 0 to 2^64 - 1
+  // mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
 
   int MOD = 17;
   int n = 3;
   cout << n << '\n';
 
   for (int i = 0; i < n; i++) {
-    cout << rand() % MOD << ' ';
+    cout << mt() % MOD << ' ';
   }
 }
